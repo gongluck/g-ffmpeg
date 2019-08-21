adc9e5de642bb36b5f2221929e4dc3dcac876a03
gongluck <1039994845@qq.com>
2019-08-21 17:59:22 +0800

    通用基类

---
+++ src/gavbase.cpp
@@ -0,0 +1,65 @@
+﻿/*******************************************************************
+*  Copyright(c) 2019
+*  All rights reserved.
+*
+*  文件名称:    gavbase.cpp
+*  简要描述:    通用基类
+*
+*  作者:  gongluck
+*  说明:
+*
+*******************************************************************/
+
+#include "gavbase.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <libavutil/opt.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+namespace gff
+{
+    // 获取锁
+    inline std::recursive_mutex& gavbase::getmutex()
+    {
+        return mutex_;
+    }
+
+    // 检查停止状态
+    inline int gavbase::checkstop() const
+    {
+        if (this->status_ != STOP)
+        {
+            av_log(nullptr, AV_LOG_ERROR, "%s %d : %ld\n", __FILE__, __LINE__, AVERROR(EINVAL));
+            return AVERROR(EINVAL);
+        }
+    }
+    inline int gavbase::checknotstop() const
+    {
+        if (this->status_ == STOP)
+        {
+            av_log(nullptr, AV_LOG_ERROR, "%s %d : %ld\n", __FILE__, __LINE__, AVERROR(EINVAL));
+            return AVERROR(EINVAL);
+        }
+    }
+
+    // 检查ffmpeg返回值
+    inline bool gavbase::checkret(int ret) const
+    {
+        if (ret < 0)
+        {
+            av_log(nullptr, ret != AVERROR(EAGAIN) ? (ret != AVERROR_EOF ? AV_LOG_ERROR : AV_LOG_INFO) : AV_LOG_DEBUG, "%s %d : %ld\n", __FILE__, __LINE__, ret);
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}// gff
---
+++ src/gavbase.h
@@ -0,0 +1,62 @@
+﻿/*******************************************************************
+*  Copyright(c) 2019
+*  All rights reserved.
+*
+*  文件名称:    gavbase.h
+*  简要描述:    通用基类
+*
+*  作者:  gongluck
+*  说明:
+*
+*******************************************************************/
+
+#ifndef __GAVBASE_H__
+#define __GAVBASE_H__
+
+#include <mutex>
+
+namespace gff
+{
+    class gavbase
+    {
+    public:
+        virtual ~gavbase() = default;
+
+        // 获取锁
+        inline std::recursive_mutex& getmutex();
+
+        // 检查停止状态
+        inline int checkstop() const;
+        inline int checknotstop() const;
+
+        // 检查ffmpeg返回值
+        inline bool checkret(int ret) const;
+
+    private:
+        // 状态
+        enum STATUS { STOP, WORKING }status_ = STOP;
+
+        // 操作锁
+        std::recursive_mutex mutex_;
+        bool locked_ = false;
+    };// gavbase
+
+    // 锁管理
+    class glock
+    {
+    public:
+        glock(std::recursive_mutex& mutex) : mutex_(mutex)
+        {
+            mutex_.lock();
+        }
+        ~glock()
+        {
+            mutex_.unlock();
+        }
+
+    private:
+        std::recursive_mutex& mutex_;
+    };// glock
+}// gff
+
+#endif//__GAVBASE_H__
