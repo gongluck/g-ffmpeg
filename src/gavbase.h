1d19f70349ea49c75c2ba1f08e7eb1742f26bb93
gongluck <1039994845@qq.com>
2019-08-21 20:30:55 +0800

    移动内联函数定义的位置到头文件

--- src/gavbase.h
+++ src/gavbase.h
@@ -13,39 +13,86 @@
 #ifndef __GAVBASE_H__
 #define __GAVBASE_H__
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <libavutil/opt.h>
+
+#ifdef __cplusplus
+}
+#endif
+
 #include <mutex>
 
 namespace gff
 {
+    typedef std::recursive_mutex gmutex;
     class gavbase
     {
     public:
         virtual ~gavbase() = default;
 
         // 获取锁
-        inline std::recursive_mutex& getmutex();
+        inline gmutex& getmutex()
+        {
+            return mutex_;
+        }
 
         // 检查停止状态
-        inline int checkstop() const;
-        inline int checknotstop() const;
+        inline bool checkstop() const
+        {
+            if (this->status_ != STOP)
+            {
+                av_log(nullptr, AV_LOG_ERROR, "%s %d : %ld\n", __FILE__, __LINE__, AVERROR(EINVAL));
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        inline bool checknotstop() const
+        {
+            if (this->status_ == STOP)
+            {
+                av_log(nullptr, AV_LOG_ERROR, "%s %d : %ld\n", __FILE__, __LINE__, AVERROR(EINVAL));
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
 
         // 检查ffmpeg返回值
-        inline bool checkret(int ret) const;
+        inline bool checkret(int ret) const
+        {
+            if (ret < 0)
+            {
+                av_log(nullptr, ret != AVERROR(EAGAIN) ? (ret != AVERROR_EOF ? AV_LOG_ERROR : AV_LOG_INFO) : AV_LOG_DEBUG, "%s %d : %ld\n", __FILE__, __LINE__, ret);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
 
     private:
         // 状态
         enum STATUS { STOP, WORKING }status_ = STOP;
 
         // 操作锁
-        std::recursive_mutex mutex_;
-        bool locked_ = false;
+        gmutex mutex_;
     };// gavbase
 
     // 锁管理
     class glock
     {
     public:
-        glock(std::recursive_mutex& mutex) : mutex_(mutex)
+        glock(gmutex& mutex) : mutex_(mutex)
         {
             mutex_.lock();
         }
@@ -55,7 +102,7 @@ namespace gff
         }
 
     private:
-        std::recursive_mutex& mutex_;
+        gmutex& mutex_;
     };// glock
 }// gff
 
